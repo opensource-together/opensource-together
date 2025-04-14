d6578af09f6a54e9eec56f63d9b1d8ab22a34c5e
Jyzdcs <ky.claudant@gmail.com>
2025-04-14 18:38:17 +0200

    feat (ost80): ajout du contrôleur et du module pour la gestion des projets, ainsi que du DTO pour la création de projets

--- apps/server/src/presentation/project/dto/CreateaProjectDtoRequest.ts
+++ apps/server/src/presentation/project/dto/CreateaProjectDtoRequest.ts
@@ -1,14 +1,4 @@
 import { IsString, IsNotEmpty, IsArray } from 'class-validator';
-import { TechStackDto } from './TechStackDto.request';
-/**
- * DTO for creating a new project
- * @class CreateProjectDtoRequest
- * @property {string} title - The title of the project
- * @property {string} description - The description of the project
- * @property {string} link - The link to the project
- * @property {'PUBLISHED' | 'DRAFT'} status - The status of the project
- * @property {TechStackDto[]} techStacks - Array of technology stacks used in the project
- */
 
 export class CreateProjectDtoRequest {
   @IsString()
@@ -29,5 +19,5 @@ export class CreateProjectDtoRequest {
 
   @IsArray()
   @IsNotEmpty()
-  techStacks: TechStackDto[];
+  techStacks: string[];
 }
--- apps/server/src/presentation/project/project.controller.ts
+++ apps/server/src/presentation/project/project.controller.ts
@@ -1,24 +1,8 @@
-import {
-  Controller,
-  Post,
-  Body,
-  Get,
-  Param,
-  Query,
-  HttpException,
-  HttpStatus,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { CreateProjectCommand } from '@/infrastructures/cqrs/project/use-case-handlers/create-project.command';
 import { CreateProjectDtoRequest } from '@/presentation/project/dto/CreateaProjectDtoRequest';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { Session } from 'supertokens-nestjs';
-import { Project } from '@/domain/project/project.entity';
-import { findProjectByTitleQuery } from '@/infrastructures/cqrs/project/queries/find-project-by-title.query';
-import { FindProjectByIdQuery } from '@/infrastructures/cqrs/project/queries/find-project-by-id.query';
-import { GetProjectsQuery } from '@/infrastructures/cqrs/project/queries/get-projects.query';
-import { toProjectResponseDto } from '@/application/dto/response/project-response.adapter';
-import { Result } from '@shared/result';
-import { ProjectResponseDto } from '@/application/dto/response/project-response.dto';
 @Controller('projects')
 export class ProjectController {
   constructor(
@@ -26,54 +10,15 @@ export class ProjectController {
     private readonly queryBus: QueryBus,
   ) {}
 
-  @Get()
-  async getProjects(): Promise<ProjectResponseDto[]> {
-    const projects: Result<Project[]> = await this.queryBus.execute(
-      new GetProjectsQuery(),
-    );
-    if (!projects.success) {
-      throw new HttpException(projects.error, HttpStatus.BAD_REQUEST);
-    }
-    return projects.value.map((project: Project) =>
-      toProjectResponseDto(project),
-    );
-  }
-
-  @Get('search')
-  async getProjectsFiltered(
-    @Query('title') title: string,
-  ): Promise<ProjectResponseDto[]> {
-    const projectsFiltered: Result<Project[]> = await this.queryBus.execute(
-      new findProjectByTitleQuery(title),
-    );
-    if (!projectsFiltered.success) {
-      throw new HttpException(projectsFiltered.error, HttpStatus.BAD_REQUEST);
-    }
-    return projectsFiltered.value.map((project: Project) =>
-      toProjectResponseDto(project),
-    );
-  }
-
-  @Get(':id')
-  async getProject(@Param('id') id: string) {
-    const projectRes: Result<Project> = await this.queryBus.execute(
-      new FindProjectByIdQuery(id),
-    );
-    if (!projectRes.success) {
-      throw new HttpException(projectRes.error, HttpStatus.NOT_FOUND);
-    }
-    console.log({ projectRes });
-    console.log(toProjectResponseDto(projectRes.value));
-    return toProjectResponseDto(projectRes.value);
-  }
-
   @Post()
   async createProject(
     @Session('userId') userId: string,
     @Body() project: CreateProjectDtoRequest,
   ) {
-    const projectRes: Result<Project> = await this.commandBus.execute(
+    console.log('Post request received !!!!!');
+    return await this.commandBus.execute(
       new CreateProjectCommand(
+        'test',
         project.title,
         project.description,
         project.link,
@@ -82,10 +27,5 @@ export class ProjectController {
         userId,
       ),
     );
-    if (!projectRes.success) {
-      throw new HttpException(projectRes.error, HttpStatus.BAD_REQUEST);
-    }
-    console.log({ projectRes });
-    return toProjectResponseDto(projectRes.value);
   }
 }
