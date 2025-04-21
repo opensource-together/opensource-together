4b9042ed4c17df5ac4469ceb30bb62319901f646
Jyzdcs <ky.claudant@gmail.com>
2025-04-22 00:49:13 +0200

    feat (ost105): renommer les méthodes de recherche de projets pour une meilleure clarté et implémenter la recherche par ID dans le dépôt Prisma

--- apps/server/src/application/ports/project.repository.port.ts
+++ apps/server/src/application/ports/project.repository.port.ts
@@ -2,7 +2,6 @@ import { Project } from '@domain/project/project.entity';
 export const PROJECT_REPOSITORY_PORT = Symbol('PROJECT_REPOSITORY_PORT');
 export interface ProjectRepositoryPort {
   save(project: Project): Promise<void>;
-  // findById(id: string): Promise<Project | null>;
-  findByTitle(title: string): Promise<Project[] | null>;
-  // findByUserId(userId: string): Promise<Project[] | null>;
+  findProjectById(id: string): Promise<Project | null>;
+  findProjectByTitle(title: string): Promise<Project[] | null>;
 }
