9a432bbf76bb1b55371f454b2832a78c1902d9bd
Jyzdcs <ky.claudant@gmail.com>
2025-04-22 01:29:54 +0200

    feat: ajout de query et son handler pour récupérer tous les projets

--- apps/server/src/infrastructures/cqrs/project/queries/get-projects.handler.ts
+++ apps/server/src/infrastructures/cqrs/project/queries/get-projects.handler.ts
@@ -2,21 +2,12 @@ import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { GetProjectsQuery } from './get-projects.query';
 import { ProjectRepositoryPort } from '@/application/ports/project.repository.port';
 import { Project } from '@/domain/project/project.entity';
-import { Inject } from '@nestjs/common';
-import { PROJECT_REPOSITORY_PORT } from '@/application/ports/project.repository.port';
-import { Result } from '@shared/result';
+
 @QueryHandler(GetProjectsQuery)
 export class GetProjectsHandler implements IQueryHandler<GetProjectsQuery> {
-  constructor(
-    @Inject(PROJECT_REPOSITORY_PORT)
-    private readonly projectRepo: ProjectRepositoryPort,
-  ) {}
+  constructor(private readonly projectRepo: ProjectRepositoryPort) {}
 
-  async execute(): Promise<Result<Project[]>> {
-    const projects = await this.projectRepo.getAllProjects();
-    if (!projects.success) {
-      return Result.fail(projects.error);
-    }
-    return Result.ok(projects.value);
+  async execute(): Promise<Project[]> {
+    return await this.projectRepo.getAllProjects();
   }
 }
