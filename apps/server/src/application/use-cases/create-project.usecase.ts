031b9a2b14e69f6418753bb9eab5f22a799c4825
Jyzdcs <ky.claudant@gmail.com>
2025-04-30 15:14:52 +0200

    feat: ajout des DTO et des mappers pour la réponse des projets, mise à jour des contrôleurs pour utiliser les DTOs lors de la récupération des projets

--- apps/server/src/application/use-cases/create-project.usecase.ts
+++ apps/server/src/application/use-cases/create-project.usecase.ts
@@ -30,6 +30,7 @@ export class CreateProjectUseCase {
     if (!project.success) {
       return Result.fail(project.error);
     }
+    console.log(project.value);
     await this.projectRepo.save(project.value);
     return Result.ok(project.value);
   }
