8e49f5ea0b7c4748fbd2ef5731c1a19c93018251
Killian <martykillian7@gmail.com>
2025-05-09 08:58:29 +0200

    feat: create ProjectCreateView for project creation page

---
+++ apps/web/src/app/project/new/page.tsx
@@ -0,0 +1,5 @@
+import ProjectCreateView from "@/features/projects/views/ProjectCreateView";
+
+export default function ProjectPage() {
+  return <ProjectCreateView />;
+}
---
+++ apps/web/src/features/projects/views/ProjectCreateView.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Header from "@/shared/layout/Header";
+
+export default function NewPageView() {
+  return (
+    <div>
+        <Header />
+      <h1>New Project</h1>
+    </div>
+  );
+}
