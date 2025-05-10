7ab9064cdd502e327f4daa6e1143bc4ec625be62
Killian <martykillian7@gmail.com>
2025-05-10 15:24:12 +0200

    restore main conflict

---
+++ apps/web/src/app/projets/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+import ProjectDetailView from '@/features/projects/views/ProjectDetailView';
+
+interface ProjectPageProps {
+  params: {
+    slug: string;
+  }
+}
+
+export default function ProjectPage({ params }: ProjectPageProps) {
+  return <ProjectDetailView params={params} />;
+} 
