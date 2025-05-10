f054edd3f923a56eb7a0adee24fa696862a7cf16
Killian <martykillian7@gmail.com>
2025-05-10 15:27:21 +0200

    fixing project page to /projects/[projectId]

--- apps/web/src/app/project/page.tsx
+++
@@ -1,7 +0,0 @@
-import ProjectDetailView from "@/features/projects/views/ProjectDetailView";
-
-export default function ProjectPage() {
-  // Fournir un slug par défaut (par exemple, "default-project")
-  const defaultParams = { slug: "default-project" };
-  return <ProjectDetailView params={defaultParams} />;
-}
