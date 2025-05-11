fdd3c3dba314b3e738581a893b60b2a312e5770e
Jyzdcs <ky.claudant@gmail.com>
2025-05-11 18:28:57 +0200

    feat: mise à jour de la configuration Docker et ajout de la documentation Swagger pour l'API

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,27 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { RootModule } from './root.module';
 import { SuperTokensExceptionFilter } from 'supertokens-nestjs';
-import supertokens from 'supertokens-node';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as YAML from 'yamljs';
+import * as swaggerUi from 'swagger-ui-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('OpenSource Together API Documentation')
-    .setDescription("Documentation de l'API")
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  app.enableCors({
-    origin: [process.env.WEBSITE_DOMAIN],
-    allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders()],
-    credentials: true,
-  });
   app.useGlobalFilters(new SuperTokensExceptionFilter());
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(document));
 
   await app.listen(process.env.PORT ?? 3000);
 }
