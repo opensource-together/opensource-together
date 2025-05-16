17de39a227d003c96b820ff839dabb962ae034a6
Jyzdcs <ky.claudant@gmail.com>
2025-05-16 23:45:50 +0200

    feat (ost151-152): ajout de la commande et du gestionnaire pour la mise à jour des projets
    - Création de `UpdateProjectCommand` pour encapsuler les données nécessaires à la mise à jour d'un projet.
    - Implémentation de `UpdateProjectCommandHandler` pour gérer l'exécution de la commande, incluant la validation et l'appel à `updateProjectById` du repository.
    - Utilisation de `UpdateProjectInputsDto` pour structurer les données de mise à jour.

---
+++ apps/server/src/application/project/commands/update/update-project.usecase.ts
@@ -0,0 +1,58 @@
+import { UpdateProjectInputsDto } from '@/application/dto/inputs/update-project-inputs.dto';
+
+import { TechStackDto } from '@/presentation/project/dto/TechStackDto.request';
+import { ICommandHandler, CommandHandler, ICommand } from '@nestjs/cqrs';
+import { ProjectRepositoryPort } from '../../ports/project.repository.port';
+import { PROJECT_REPOSITORY_PORT } from '@/application/project/ports/project.repository.port';
+import { Inject } from '@nestjs/common';
+import { Result } from '@/shared/result';
+import { Project } from '@/domain/project/project.entity';
+import { Title } from '@/domain/project/title/title.vo';
+import { Description } from '@/domain/project/description/description.vo';
+import { Link } from '@/domain/project/link/link.vo';
+import { Status } from '@/domain/project/status/status.vo';
+
+export class UpdateProjectCommand implements ICommand {
+  constructor(
+    public readonly id: string,
+    public readonly title: string | null,
+    public readonly description: string | null,
+    public readonly link: string | null,
+    public readonly status: string | null,
+    public readonly techStacks: TechStackDto[],
+    public readonly userId: string,
+  ) {}
+}
+
+@CommandHandler(UpdateProjectCommand)
+export class UpdateProjectCommandHandler
+  implements ICommandHandler<UpdateProjectCommand>
+{
+  constructor(
+    @Inject(PROJECT_REPOSITORY_PORT)
+    private readonly projectRepo: ProjectRepositoryPort,
+  ) {}
+
+  async execute(command: UpdateProjectCommand): Promise<Result<Project>> {
+    const updatePayload: UpdateProjectInputsDto = {
+      title: command.title ? Title.create(command.title) : undefined,
+      description: command.description
+        ? Description.create(command.description)
+        : undefined,
+      link: command.link ? Link.create(command.link) : undefined,
+      status: command.status ? Status.create(command.status) : undefined,
+      techStacks: command.techStacks,
+    };
+
+    const project = await this.projectRepo.updateProjectById(
+      command.id,
+      updatePayload,
+      command.userId,
+    );
+
+    if (!project.success) {
+      return Result.fail(project.error);
+    }
+    return Result.ok(project.value);
+  }
+}
