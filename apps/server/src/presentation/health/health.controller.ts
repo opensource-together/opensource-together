a69a0209ca4efd86301860e70260d8ca01e3cd1a
Cédric Sazos <cedric.sazos@tutanota.com>
2025-05-22 16:19:26 +0200

    feat: add health controller

---
+++ apps/server/src/presentation/health/health.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheckService, HttpHealthIndicator, HealthCheck } from '@nestjs/terminus';
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.http.responseCheck(
+        'main-page',
+        process.env.API_DOMAIN || "",
+        (res) => res.status === 200,
+      ),
+    ]);
+  }
+
+}
+
