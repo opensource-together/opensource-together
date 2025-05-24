a8768dc4f4653c9078e57f8c81e8ece284163734
Olyxz16 <cedric.sazos@tutanota.com>
2025-05-24 20:00:34 +0200

    fix: wrong health check command

--- apps/server/src/presentation/health/health.controller.ts
+++ apps/server/src/presentation/health/health.controller.ts
@@ -1,29 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { HealthCheckService, HttpHealthIndicator, HealthCheck } from '@nestjs/terminus';
+import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
 import { PublicAccess } from 'supertokens-nestjs';
 
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
-    private http: HttpHealthIndicator,
-    private config: ConfigService,
   ) {}
 
   @PublicAccess()
   @Get()
   @HealthCheck()
   check() {
-    return this.health.check([
-      () => this.http.responseCheck(
-        'server',
-        this.config.get("API_DOMAIN") || "",
-        (res) => {
-          return res.status === 404;
-        },
-      ),
-    ]);
+    return this.health.check([]);
   }
 
 }
