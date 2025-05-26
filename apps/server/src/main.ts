0976a21a13331aca8bd8acc2e69400e2ee3b8d24
y2_znt <yonideserbaix@gmail.com>
2025-05-26 23:08:10 +0200

    refactor: clean up main.ts and comment out Swagger documentation setup

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,27 +1,26 @@
 import { NestFactory } from '@nestjs/core';
-import { RootModule } from './root.module';
 import { SuperTokensExceptionFilter } from 'supertokens-nestjs';
-import * as YAML from 'yamljs';
-import * as swaggerUi from 'swagger-ui-express';
 import supertokens from 'supertokens-node';
+import { RootModule } from './root.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
 
   // Configuration CORS
   app.enableCors({
-    origin: [process.env.WEBSITE_DOMAIN || 'http://localhost:3000'],
+    origin: ['http://localhost:3000'],
     allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders()],
     credentials: true,
   });
 
-  const document = YAML.load('swagger-doc.example.yml');
+  // const document = YAML.load('swagger-doc.example.yml');
 
   app.useGlobalFilters(new SuperTokensExceptionFilter());
 
   if (process.env.NODE_ENV !== 'PRODUCTION') {
-    const document = YAML.load('swagger-doc.example.yml');
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(document));
+    // const document = YAML.load('swagger-doc.example.yml');
+    // const document = YAML.load('swagger-doc.yml');
+    // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(document));
   }
 
   await app.listen(process.env.PORT ?? 4000);
