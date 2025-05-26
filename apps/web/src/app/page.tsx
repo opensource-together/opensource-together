d98f825dd0c3f93d63fba32e458d7d19f740071f
lhourquin <lhourquin@gmail.com>
2025-05-26 09:36:49 +0200

    refactor: enhance authentication components and structure
    - Reorganized layout components to improve rendering order and maintainability.
    - Replaced direct button implementation in LoginForm with GitHubButton for better reusability.
    - Introduced AuthCard component to standardize the layout for authentication views.
    - Created separate views for Login and SignUp, encapsulating their respective forms within AuthCard.
    - Added SignUpForm and GitHubButton components to streamline user authentication processes.
    - Improved code readability and structure by utilizing functional components effectively.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
-
-import LoginForm from "@/features/auth/components/LoginForm";
 import HomepageViews from "@/features/projects/views/HomepageViews";
 
 export default function HomePage() {
   return (
     <main>
-      <LoginForm />
       {/*Your app components*/}
       <HomepageViews />
     </main>
