379753e28b9605b5b10d8445a648926a0c30ec07
Killian <martykillian7@gmail.com>
2025-05-28 19:15:36 +0200

    feat: add layout and images to login page

--- apps/web/src/features/auth/components/AuthCard.tsx
+++ apps/web/src/features/auth/components/AuthCard.tsx
@@ -8,9 +8,9 @@ interface AuthCardProps {
 
 export default function AuthCard({ title, subtitle, children }: AuthCardProps) {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center ">
       <div className="w-full max-w-md">
-        <div className=" p-4">
+        <div className="p-4">
           <div className="mb-8 text-center">
             <h1 className="mb-3 text-[24px] font-medium tracking-tight text-gray-900">
               {title}
