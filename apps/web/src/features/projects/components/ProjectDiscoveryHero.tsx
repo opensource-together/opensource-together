8d419a53138022c5a25a3ab30823637b52f4915f
y2_znt <yonideserbaix@gmail.com>
2025-05-30 01:50:41 +0200

    feat: add Aspekta font and integrate into ProjectDiscoveryHero component
    - Introduced Aspekta-500 font file and defined font-face in CSS.
    - Updated layout to include font-face styles.
    - Modified ProjectDiscoveryHero component to utilize Aspekta font for improved typography.

--- apps/web/src/features/projects/components/ProjectDiscoveryHero.tsx
+++ apps/web/src/features/projects/components/ProjectDiscoveryHero.tsx
@@ -6,8 +6,7 @@ export default function ProjectDiscoveryHero() {
   return (
     <div className="relative mx-auto rounded-4xl border border-black/5">
       <div className="absolute inset-0 flex flex-col items-center justify-center">
-        <p className="text-muted-foreground flex items-center gap-1 text-center text-xs font-normal tracking-tight">
-          {" "}
+        <span className="text-muted-foreground mt-6 flex items-center gap-1 text-center text-xs font-normal tracking-tight md:mt-0">
           <Image
             src="/icons/blue-point-new-icon.svg"
             alt="new-project"
@@ -15,22 +14,28 @@ export default function ProjectDiscoveryHero() {
             height={10}
           />
           10 Projets ajoutés cette semaine !
-        </p>
-        <p className="mt-3 text-center text-5xl leading-none font-medium tracking-tighter">
+        </span>
+        <h1
+          className="mt-3 text-center text-3xl leading-none tracking-tighter md:text-5xl"
+          style={{ fontFamily: "Aspekta", fontWeight: 500 }}
+        >
           Grandir ensemble <br />
           grâce à l'open source
-        </p>
-        <p className="text-muted-foreground mt-4 mb-6 text-center text-sm">
+        </h1>
+        <p className="text-muted-foreground mx-7 mt-4 mb-6 max-w-[450px] text-center text-xs md:text-sm">
           Trouvez des projets, postulez à des rôles, collaborez — construisons,{" "}
-          <br /> partageons et grandissons ensemble grâce à l'open source
+          partageons et grandissons ensemble grâce à l'open source
         </p>
-        <FilterSearchBar />
+        <div className="hidden md:block">
+          <FilterSearchBar />
+        </div>
       </div>
       <Image
         src="/background-homepage.png"
         alt="billboard"
         width={1108}
         height={330}
+        className="h-[200px] w-full md:h-[330px]"
       />
     </div>
   );
