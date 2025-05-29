46d66dcfffa47dd92497e7bc793d87d7c377cc20
y2_znt <yonideserbaix@gmail.com>
2025-05-29 19:21:50 +0200

    feat: update Billboard component with improved styling and text semantics
    - Refactored the Billboard component to enhance visual presentation and accessibility.
    - Updated class names for better styling consistency and readability.
    - Improved text semantics by using appropriate font sizes and styles.

--- apps/web/src/components/shared/Billboard.tsx
+++ apps/web/src/components/shared/Billboard.tsx
@@ -1,16 +1,28 @@
 import Image from "next/image";
+
 import FilterSearchBar from "../ui/filterSearchBar";
 
 export default function Billboard() {
   return (
-    <div className="relative h-[330px] w-[1108px] rounded-[30px] border border-black/5">
+    <div className="relative mx-auto rounded-4xl border border-black/5">
       <div className="absolute inset-0 flex flex-col items-center justify-center">
-        <p className="font-geist flex items-center gap-1 font-normal text-[10px] text-center text-black/50"> <Image src="/icons/blue-point-new-icon.svg" alt="new-project" width={10} height={10} />10 Projets ajoutés cette semaine !</p>
-        <p className="font-geist font-medium text-[44px] text-center text-black leading-none tracking-[-0.03em] mt-3">
-          Grandir ensemble <br />grâce à l'open source
+        <p className="text-muted-foreground flex items-center gap-1 text-center text-xs font-normal tracking-tight">
+          {" "}
+          <Image
+            src="/icons/blue-point-new-icon.svg"
+            alt="new-project"
+            width={10}
+            height={10}
+          />
+          10 Projets ajoutés cette semaine !
         </p>
-        <p className="font-geist font-normal text-[12px] text-center text-black/50 mt-4 mb-6">
-          Trouvez des projets, postulez à des rôles, collaborez — construisons, <br /> partageons et grandissons ensemble grâce à l'open source
+        <p className="mt-3 text-center text-5xl leading-none font-medium tracking-tight">
+          Grandir ensemble <br />
+          grâce à l'open source
+        </p>
+        <p className="text-muted-foreground mt-4 mb-6 text-center text-sm">
+          Trouvez des projets, postulez à des rôles, collaborez — construisons,{" "}
+          <br /> partageons et grandissons ensemble grâce à l'open source
         </p>
         <FilterSearchBar />
       </div>
