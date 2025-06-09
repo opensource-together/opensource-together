4af7bf396d790a7e275263a0599708f2213dc34c
Jyzdcs <ky.claudant@gmail.com>
2025-06-09 19:54:54 +0200

    feat: ajout d'un nouveau DTO pour le filtrage des projets et mise à jour de l'interface du dépôt
    - Création de l'interface `ProjectFilterInputsDto` pour permettre le filtrage des projets par titre, difficulté, ordre de tri, rôles et technologies.
    - Mise à jour de l'interface `ProjectRepositoryPort` pour inclure la méthode `findProjectByFilters` utilisant le nouveau DTO.

--- apps/server/src/application/project/ports/project.repository.port.ts
+++ apps/server/src/application/project/ports/project.repository.port.ts
@@ -1,6 +1,7 @@
 import { Result } from '@/shared/result';
 import { Project } from '@domain/project/project.entity';
 import { UpdateProjectInputsDto } from '@/application/dto/inputs/update-project-inputs.dto';
+import { ProjectFilterInputsDto } from '@/application/dto/inputs/filter-project-input';
 export const PROJECT_REPOSITORY_PORT = Symbol('PROJECT_REPOSITORY_PORT');
 export interface ProjectRepositoryPort {
   save(project: Project): Promise<Result<Project>>;
@@ -10,6 +11,8 @@ export interface ProjectRepositoryPort {
     userId: string,
   ): Promise<Result<Project>>;
   findProjectById(id: string): Promise<Result<Project | null>>;
-  findProjectByTitle(title: string): Promise<Result<Project[] | null>>;
+  findProjectByFilters(
+    filters: ProjectFilterInputsDto,
+  ): Promise<Result<Project[] | null>>;
   getAllProjects(): Promise<Result<Project[]>>;
 }
