a6f7659785eee1dfe2d0c36cecf783a96f0703fe
lhourquin <lhourquin@gmail.com>
2025-06-10 07:29:38 +0200

    feat: implement user update command with encryption for GitHub access token
    - Added UpdateGithubTokenUserCommand and its handler to facilitate updating user GitHub access tokens.
    - Integrated encryption service to securely handle GitHub access tokens during user updates.
    - Updated user commands container to include the new update command and handler.
    - Enhanced user wiring module to provide the encryption service for dependency injection.

--- apps/server/src/application/user/commands/user.commands.ts
+++ apps/server/src/application/user/commands/user.commands.ts
@@ -2,7 +2,14 @@ import {
   CreateUserCommand,
   CreateUserCommandHandler,
 } from './create-user.command';
+import {
+  UpdateGithubTokenUserCommand,
+  UpdateGithubTokenUserCommandHandler,
+} from './update-user.command';
+
 export const userCommandsContainer = [
   CreateUserCommand,
   CreateUserCommandHandler,
+  UpdateGithubTokenUserCommand,
+  UpdateGithubTokenUserCommandHandler,
 ];
