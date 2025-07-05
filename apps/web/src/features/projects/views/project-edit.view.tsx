43f43c31162cc405de90e62008cc7847d3c27206
y2_znt <yonideserbaix@gmail.com>
2025-07-05 15:42:09 +0200

    refactor: remove ProjectEditSidebar and related right sidebar components
    - Deleted ProjectEditSidebar component and its associated right sidebar components to streamline the project editing interface.
    - Updated ProjectEditView to remove the sidebar integration, simplifying the layout.

--- apps/web/src/features/projects/views/project-edit.view.tsx
+++ apps/web/src/features/projects/views/project-edit.view.tsx
@@ -3,7 +3,6 @@
 import Breadcrumb from "@/shared/components/shared/Breadcrumb";
 
 import ProjectEditFormError from "../components/error-ui/project-edit-form-error.component";
-import ProjectEditSidebar from "../components/project-edit-sidebar.component";
 import SkeletonProjectEditForm from "../components/skeletons/skeleton-project-edit-form.component";
 import ProjectEditForm from "../forms/project-edit.form";
 import { useProject } from "../hooks/use-projects.hook";
@@ -28,7 +27,6 @@ export default function ProjectEditView({ projectId }: { projectId: string }) {
       <div className="mx-auto mt-4 max-w-[1300px] px-4 sm:px-6 md:mt-8 md:px-8 lg:px-24 xl:px-40">
         <div className="flex flex-col items-start justify-between gap-4 lg:flex-row lg:gap-16">
           <ProjectEditForm project={project} />
-          <ProjectEditSidebar project={project} />
         </div>
       </div>
     </>
