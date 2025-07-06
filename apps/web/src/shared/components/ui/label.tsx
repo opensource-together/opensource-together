6a5e92b0a0907552694b28a4464ea978cc2e46ee
y2_znt <yonideserbaix@gmail.com>
2025-07-06 09:17:18 +0200

    fix: remove unnecessary margin from Label component
    - Removed the bottom margin class from the Label component to streamline its styling and improve layout consistency.

--- apps/web/src/shared/components/ui/label.tsx
+++ apps/web/src/shared/components/ui/label.tsx
@@ -19,7 +19,7 @@ function Label({
     <LabelPrimitive.Root
       data-slot="label"
       className={cn(
-        "mb-2 flex items-center gap-2 leading-none font-medium tracking-tighter select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+        "flex items-center gap-2 leading-none font-medium tracking-tighter select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
         className
       )}
       {...props}
