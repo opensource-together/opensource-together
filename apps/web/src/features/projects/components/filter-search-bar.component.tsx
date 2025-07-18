aaa37e1fb38dc48d6b76b71804b54d0504f7f290
Killian <martykillian7@gmail.com>
2025-07-18 11:46:34 +0200

    feature/filter-search-bar-style

--- apps/web/src/features/projects/components/filter-search-bar.component.tsx
+++ apps/web/src/features/projects/components/filter-search-bar.component.tsx
@@ -8,38 +8,42 @@ interface FilterItemProps {
 
 function FilterItem({ label, value }: FilterItemProps) {
   return (
-    <div className="flex cursor-pointer flex-col rounded-full px-4 py-1 transition-colors hover:bg-gray-50">
-      <span className="text-[11px] font-normal text-black/50">{label}</span>
-      <span className="text-xs font-medium tracking-tight">{value}</span>
+    <div className="group flex h-[50px] w-[125px] cursor-pointer flex-col rounded-full px-4 py-[8px] transition-all duration-200 hover:bg-white">
+      <span className="text-[11px] font-normal text-black/50 transition-colors duration-200 group-hover:text-black/70">
+        {label}
+      </span>
+      <span className="text-xs font-medium tracking-tight transition-colors duration-200 group-hover:text-black">
+        {value}
+      </span>
     </div>
   );
 }
 
 export default function FilterSearchBar() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="flex items-center gap-4 rounded-full border border-black/5 bg-white px-2 py-1 shadow-md shadow-black/3 backdrop-blur-lg">
-        <div className="flex items-center gap-6">
+    <div className="flex h-[50px] w-[649px] items-center justify-center">
+      <div className="group flex h-full w-full items-center justify-between rounded-full border border-black/5 bg-white px-4 py-1 shadow-md shadow-black/3 backdrop-blur-lg transition-colors duration-200 hover:bg-black/5">
+        <div className="absolute left-[-1px] flex items-center gap-4">
           <div className="flex items-center">
-            <div className="relative pr-4">
+            <div className="relative">
               <FilterItem label="Filtrer par" value="Technologie" />
-              <div className="absolute top-1/2 right-0 h-8 -translate-y-1/2" />
+              <div className="absolute top-1/2 right-0 h-12 -translate-y-1/2" />
             </div>
-            <div className="relative px-4">
+            <div className="relative">
               <FilterItem label="Filtrer par" value="Role" />
               <div className="absolute top-1/2 right-0 h-8 -translate-y-1/2" />
             </div>
-            <div className="relative px-4">
+            <div className="relative">
               <FilterItem label="Filtrer par" value="Catégorie" />
               <div className="absolute top-1/2 right-0 h-8 -translate-y-1/2" />
             </div>
-            <div className="pl-4">
+            <div className="">
               <FilterItem label="Trier par" value="Plus Récent" />
             </div>
           </div>
         </div>
 
-        <Button className="px-4">
+        <Button className="absolute right-2 h-[35px] px-4">
           Chercher un Projet{" "}
           <Icon
             name="search"
