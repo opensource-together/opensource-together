86fd564f4b6c56067bd93668a5d1bab901d5c00a
y2_znt <yonideserbaix@gmail.com>
2025-07-25 21:40:44 +0200

    refactor: simplify dashboard layout and restructure pages
    - Removed the DashboardClientProviderWrapper from the DashboardLayout component.
    - Updated DashboardPage to use HomeDashboardView instead of DashboardView.
    - Deleted unused pages: applications, contributions, and projects.
    - Added new pages for My Applications, My Contributions, and My Projects with corresponding views.
    - Introduced a new DashboardHeading component for better UI consistency.

--- apps/web/src/app/dashboard/layout.tsx
+++ apps/web/src/app/dashboard/layout.tsx
@@ -1,15 +1,12 @@
 import { ReactNode } from "react";
 
-import DashboardClientProviderWrapper from "@/features/dashboard/components/dashboard-client-provider-wrapper";
 import DashboardSidebar from "@/features/dashboard/components/dashboard-sidebar.component";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <DashboardClientProviderWrapper>
-      <div className="flex h-[calc(100vh-65px)] overflow-hidden md:h-[calc(100vh-85px)]">
-        <DashboardSidebar />
-        <main className="flex-1 overflow-y-auto px-14 pt-12">{children}</main>
-      </div>
-    </DashboardClientProviderWrapper>
+    <div className="flex h-[calc(100vh-65px)] overflow-hidden md:h-[calc(100vh-85px)]">
+      <DashboardSidebar />
+      <main className="flex-1 overflow-y-auto px-14 pt-12">{children}</main>
+    </div>
   );
 }
