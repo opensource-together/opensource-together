95db3edd25b50e229046269db95cc583edf4b673
y2_znt <yonideserbaix@gmail.com>
2025-07-26 00:23:30 +0200

    feat: enhance dashboard with project and application management features
    - Updated protected routes to include the dashboard.
    - Refactored ApplicationCard component to use applicationId for better clarity.
    - Introduced MyProjectsList component to display user projects with an empty state.
    - Enhanced MyApplicationsList component with filtering options for application statuses.
    - Updated ProjectApplicationsList to utilize new hooks for project role applications.
    - Created MyProjectsView to showcase user projects and received applications.

--- apps/web/src/features/dashboard/views/my-applications.view.tsx
+++ apps/web/src/features/dashboard/views/my-applications.view.tsx
@@ -1,4 +1,5 @@
 import DashboardHeading from "../components/dashboard-heading.component";
+import MyApplicationsList from "../components/my-applications-list.component";
 
 export default function MyApplicationsView() {
   return (
@@ -7,6 +8,9 @@ export default function MyApplicationsView() {
         title="Mes candidatures"
         description="Vous pouvez visualiser vos candidatures de projets Open Source ici."
       />
+      <div className="mt-10 flex flex-col gap-4">
+        <MyApplicationsList />
+      </div>
     </div>
   );
 }
