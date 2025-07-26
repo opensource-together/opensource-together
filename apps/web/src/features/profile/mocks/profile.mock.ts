5fd3f3f8c3134e97a7b50dfce61c89c41c6fcd60
y2_znt <yonideserbaix@gmail.com>
2025-07-27 00:54:24 +0200

    refactor: remove unused formatJoinDate function from ProfileHero component and comment out mockProfile data in profile mock file

--- apps/web/src/features/profile/mocks/profile.mock.ts
+++ apps/web/src/features/profile/mocks/profile.mock.ts
@@ -1,5 +1,3 @@
-import { Profile } from "../types/profile.type";
-
 // export const mockProfiles: Profile[] = [
 //   {
 //     id: "1",
@@ -158,204 +156,204 @@ import { Profile } from "../types/profile.type";
 //   },
 // ];
 
-export const mockProfile: Profile = {
-  id: "1",
-  login: "byronlove111",
-  avatarUrl:
-    "https://pbs.twimg.com/profile_images/1813513692471779328/6RxAJKDu_400x400.jpg",
-  websiteUrl: "https://github.com/byronlove111",
-  type: "User",
-  name: "Byron Love",
-  bio: "Développeur fullstack créant du code propre et efficace pour des expériences web modernes.",
-  location: "Paris, France",
-  blog: "https://byronlove111.dev",
-  company: "Freelance",
-  joinedAt: "2025-04-25T00:00:00Z",
-  contributionsCount: 1268,
-  socialLinks: {
-    github: "https://github.com/byronlove111",
-    twitter: "https://x.com/byronlove111",
-    linkedin: "https://www.linkedin.com/in/byronlove111/",
-    discord: "https://discord.com/byronlove111",
-    website: "https://byronlove111.dev",
-  },
-  links: [
-    {
-      type: "github",
-      url: "https://github.com/byronlove111",
-    },
-    {
-      type: "link",
-      url: "https://byronlove111.dev",
-    },
-    {
-      type: "twitter",
-      url: "https://x.com/byronlove111",
-    },
-    {
-      type: "linkedin",
-      url: "https://www.linkedin.com/in/byronlove111/",
-    },
-    {
-      type: "discord",
-      url: "https://discord.com/byronlove111",
-    },
-  ],
-  techStacks: [
-    {
-      id: "1",
-      name: "React",
-      iconUrl:
-        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
-      type: "TECH",
-    },
-    {
-      id: "2",
-      name: "Figma",
-      iconUrl:
-        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg",
-      type: "TECH",
-    },
-    {
-      id: "3",
-      name: "MongoDB",
-      iconUrl:
-        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg",
-      type: "TECH",
-    },
-  ],
-  projects: [
-    {
-      id: "1",
-      name: "LeetGrind",
-      full_name: "LeetGrind/LeetGrind",
-      description:
-        "Un bot Discord pour pratiquer LeetCode chaque jour et progresser en algorithme dans une ambiance motivante et collaborative, avec des défis quotidiens et un système de classement pour suivre votre évolution",
-      html_url: "https://github.com/LeetGrind/LeetGrind",
-      homepage: "https://www.leetgrindbot.com/",
-      stargazers_count: 156,
-      watchers_count: 156,
-      forks_count: 23,
-      language: "TypeScript",
-      topics: ["discord-bot", "typescript", "developer-tools"],
-      created_at: "2024-01-01T00:00:00Z",
-      updated_at: "2024-03-19T00:00:00Z",
-      pushed_at: "2024-03-19T00:00:00Z",
-      image:
-        "https://www.google.com/s2/favicons?sz=256&domain_url=https%3A%2F%2Fwww.leetgrindbot.com%2F",
-      techStacks: [
-        {
-          id: "1",
-          name: "TypeScript",
-          iconUrl:
-            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
-          type: "LANGUAGE",
-        },
-      ],
-      author: {
-        ownerId: "1",
-        name: "LeetGrind",
-        avatarUrl: "https://github.com/LeetGrind.png",
-      },
-      projectStats: {
-        contributors: [
-          { login: "y2_dev", avatar_url: "", html_url: "", contributions: 10 },
-        ],
-        stars: 100,
-        forks: 10,
-        lastCommit: {
-          date: new Date("2024-01-01T00:00:00Z").toISOString(),
-          message: "Initial commit",
-          sha: "1234567890",
-          url: "https://github.com/LeetGrind/LeetGrind/commit/1234567890",
-          author: {
-            login: "y2_dev",
-            avatar_url: "",
-            html_url: "",
-          },
-        },
-      },
-    },
-    {
-      id: "2",
-      name: "devcord",
-      full_name: "devcord-bot/devcord",
-      description:
-        "Bot Discord pour les développeurs avec des commandes intelligentes",
-      html_url: "https://github.com/devcord-bot/devcord",
-      homepage: "https://docs.devcord.app/introduction",
-      stargazers_count: 156,
-      watchers_count: 156,
-      forks_count: 23,
-      language: "Go",
-      topics: ["discord-bot", "golang", "developer-tools"],
-      created_at: "2024-01-01T00:00:00Z",
-      updated_at: "2024-03-19T00:00:00Z",
-      pushed_at: "2024-03-19T00:00:00Z",
-      image:
-        "https://www.google.com/s2/favicons?sz=256&domain_url=https%3A%2F%2Fdocs.devcord.app%2Fintroduction",
-      techStacks: [
-        {
-          id: "2",
-          name: "TypeScript",
-          iconUrl:
-            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
-          type: "LANGUAGE",
-        },
-        {
-          id: "1",
-          name: "Go",
-          iconUrl:
-            "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/go/go-original-wordmark.svg",
-          type: "LANGUAGE",
-        },
-      ],
-      author: {
-        ownerId: "1",
-        name: "devcord",
-        avatarUrl: "https://github.com/devcord-bot.png",
-      },
-      projectStats: {
-        contributors: [
-          { login: "y2_dev", avatar_url: "", html_url: "", contributions: 10 },
-        ],
-        stars: 100,
-        forks: 10,
-        lastCommit: {
-          date: new Date("2024-01-01T00:00:00Z").toISOString(),
-          message: "Initial commit",
-          sha: "1234567890",
-          url: "https://github.com/devcord-bot/devcord/commit/1234567890",
-          author: {
-            login: "y2_dev",
-            avatar_url: "",
-            html_url: "",
-          },
-        },
-      },
-    },
-  ],
-  experiences: [
-    {
-      id: "1",
-      company: "OpenSource Together",
-      position: "Lead Developer",
-      startDate: "2024-01-01",
-      endDate: "2025",
-    },
-    {
-      id: "2",
-      company: "LeetGrind",
-      position: "Lead Developer",
-      startDate: "2023-01-01",
-      endDate: "2024-03-19",
-    },
-    {
-      id: "3",
-      company: "Project",
-      position: "Intern",
-      startDate: "2021-01-01",
-      endDate: "2023-01-01",
-    },
-  ],
-};
+// export const mockProfile: Profile = {
+//   id: "1",
+//   login: "byronlove111",
+//   avatarUrl:
+//     "https://pbs.twimg.com/profile_images/1813513692471779328/6RxAJKDu_400x400.jpg",
+//   websiteUrl: "https://github.com/byronlove111",
+//   type: "User",
+//   name: "Byron Love",
+//   bio: "Développeur fullstack créant du code propre et efficace pour des expériences web modernes.",
+//   location: "Paris, France",
+//   blog: "https://byronlove111.dev",
+//   company: "Freelance",
+//   joinedAt: "2025-04-25T00:00:00Z",
+//   contributionsCount: 1268,
+//   socialLinks: {
+//     github: "https://github.com/byronlove111",
+//     twitter: "https://x.com/byronlove111",
+//     linkedin: "https://www.linkedin.com/in/byronlove111/",
+//     discord: "https://discord.com/byronlove111",
+//     website: "https://byronlove111.dev",
+//   },
+//   links: [
+//     {
+//       type: "github",
+//       url: "https://github.com/byronlove111",
+//     },
+//     {
+//       type: "link",
+//       url: "https://byronlove111.dev",
+//     },
+//     {
+//       type: "twitter",
+//       url: "https://x.com/byronlove111",
+//     },
+//     {
+//       type: "linkedin",
+//       url: "https://www.linkedin.com/in/byronlove111/",
+//     },
+//     {
+//       type: "discord",
+//       url: "https://discord.com/byronlove111",
+//     },
+//   ],
+//   techStacks: [
+//     {
+//       id: "1",
+//       name: "React",
+//       iconUrl:
+//         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+//       type: "TECH",
+//     },
+//     {
+//       id: "2",
+//       name: "Figma",
+//       iconUrl:
+//         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg",
+//       type: "TECH",
+//     },
+//     {
+//       id: "3",
+//       name: "MongoDB",
+//       iconUrl:
+//         "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg",
+//       type: "TECH",
+//     },
+//   ],
+//   projects: [
+//     {
+//       id: "1",
+//       name: "LeetGrind",
+//       full_name: "LeetGrind/LeetGrind",
+//       description:
+//         "Un bot Discord pour pratiquer LeetCode chaque jour et progresser en algorithme dans une ambiance motivante et collaborative, avec des défis quotidiens et un système de classement pour suivre votre évolution",
+//       html_url: "https://github.com/LeetGrind/LeetGrind",
+//       homepage: "https://www.leetgrindbot.com/",
+//       stargazers_count: 156,
+//       watchers_count: 156,
+//       forks_count: 23,
+//       language: "TypeScript",
+//       topics: ["discord-bot", "typescript", "developer-tools"],
+//       created_at: "2024-01-01T00:00:00Z",
+//       updated_at: "2024-03-19T00:00:00Z",
+//       pushed_at: "2024-03-19T00:00:00Z",
+//       image:
+//         "https://www.google.com/s2/favicons?sz=256&domain_url=https%3A%2F%2Fwww.leetgrindbot.com%2F",
+//       techStacks: [
+//         {
+//           id: "1",
+//           name: "TypeScript",
+//           iconUrl:
+//             "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+//           type: "LANGUAGE",
+//         },
+//       ],
+//       author: {
+//         ownerId: "1",
+//         name: "LeetGrind",
+//         avatarUrl: "https://github.com/LeetGrind.png",
+//       },
+//       projectStats: {
+//         contributors: [
+//           { login: "y2_dev", avatar_url: "", html_url: "", contributions: 10 },
+//         ],
+//         stars: 100,
+//         forks: 10,
+//         lastCommit: {
+//           date: new Date("2024-01-01T00:00:00Z").toISOString(),
+//           message: "Initial commit",
+//           sha: "1234567890",
+//           url: "https://github.com/LeetGrind/LeetGrind/commit/1234567890",
+//           author: {
+//             login: "y2_dev",
+//             avatar_url: "",
+//             html_url: "",
+//           },
+//         },
+//       },
+//     },
+//     {
+//       id: "2",
+//       name: "devcord",
+//       full_name: "devcord-bot/devcord",
+//       description:
+//         "Bot Discord pour les développeurs avec des commandes intelligentes",
+//       html_url: "https://github.com/devcord-bot/devcord",
+//       homepage: "https://docs.devcord.app/introduction",
+//       stargazers_count: 156,
+//       watchers_count: 156,
+//       forks_count: 23,
+//       language: "Go",
+//       topics: ["discord-bot", "golang", "developer-tools"],
+//       created_at: "2024-01-01T00:00:00Z",
+//       updated_at: "2024-03-19T00:00:00Z",
+//       pushed_at: "2024-03-19T00:00:00Z",
+//       image:
+//         "https://www.google.com/s2/favicons?sz=256&domain_url=https%3A%2F%2Fdocs.devcord.app%2Fintroduction",
+//       techStacks: [
+//         {
+//           id: "2",
+//           name: "TypeScript",
+//           iconUrl:
+//             "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+//           type: "LANGUAGE",
+//         },
+//         {
+//           id: "1",
+//           name: "Go",
+//           iconUrl:
+//             "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/go/go-original-wordmark.svg",
+//           type: "LANGUAGE",
+//         },
+//       ],
+//       author: {
+//         ownerId: "1",
+//         name: "devcord",
+//         avatarUrl: "https://github.com/devcord-bot.png",
+//       },
+//       projectStats: {
+//         contributors: [
+//           { login: "y2_dev", avatar_url: "", html_url: "", contributions: 10 },
+//         ],
+//         stars: 100,
+//         forks: 10,
+//         lastCommit: {
+//           date: new Date("2024-01-01T00:00:00Z").toISOString(),
+//           message: "Initial commit",
+//           sha: "1234567890",
+//           url: "https://github.com/devcord-bot/devcord/commit/1234567890",
+//           author: {
+//             login: "y2_dev",
+//             avatar_url: "",
+//             html_url: "",
+//           },
+//         },
+//       },
+//     },
+//   ],
+//   experiences: [
+//     {
+//       id: "1",
+//       company: "OpenSource Together",
+//       position: "Lead Developer",
+//       startDate: "2024-01-01",
+//       endDate: "2025",
+//     },
+//     {
+//       id: "2",
+//       company: "LeetGrind",
+//       position: "Lead Developer",
+//       startDate: "2023-01-01",
+//       endDate: "2024-03-19",
+//     },
+//     {
+//       id: "3",
+//       company: "Project",
+//       position: "Intern",
+//       startDate: "2021-01-01",
+//       endDate: "2023-01-01",
+//     },
+//   ],
+// };
