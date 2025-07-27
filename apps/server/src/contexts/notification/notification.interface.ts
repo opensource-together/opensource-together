6e04d6c7f72ee115e7788aab4b0c3ac76021898b
Jyzdcs <ky.claudant@gmail.com>
2025-07-27 13:08:38 +0200

    feat: ajout d'interfaces pour la gestion des notifications de candidature et mise à jour du modèle Notification
    - Création de nouvelles interfaces pour les notifications liées aux candidatures de projet, incluant les états de la candidature (acceptée, rejetée, créée).
    - Ajout d'un champ `object` au modèle Notification pour stocker des informations supplémentaires.
    - Ajout d'une migration SQL pour intégrer le nouveau champ dans la base de données.

---
+++ apps/server/src/contexts/notification/notification.interface.ts
@@ -0,0 +1,80 @@
+import { ProjectRole, Profile } from '@prisma/client';
+
+interface BaseNotification {
+  object: string;
+  message: string;
+}
+
+interface ApplyProjectRequest extends BaseNotification {
+  appplicationId: string;
+  projectRoleId: string;
+  projectRoleTitle: string;
+  project: {
+    id: string;
+    title: string;
+    shortDescription: string;
+    image?: string;
+    author: Profile;
+  };
+  projectRole: ProjectRole;
+  status: 'PENDING' | 'ACCEPTED' | 'REJECTED' | 'CANCELLED';
+  selectedKeyFeatures: {
+    feature: string;
+  }[];
+  selectedProjectGoals: {
+    goal: string;
+  }[];
+  appliedAt: Date;
+  decidedAt?: Date;
+  decidedBy?: string;
+  rejectionReason?: string;
+  motivationLetter?: string;
+  userProfile: {
+    id: string;
+    name: string;
+    avatarUrl?: string;
+  };
+}
+
+export interface ApplyProjectRequestAcceptedNotification
+  extends ApplyProjectRequest {
+  type: 'project.role.application.accepted';
+}
+
+export interface ApplyProjectRequestRejectedNotification
+  extends ApplyProjectRequest {
+  type: 'project.role.application.rejected';
+}
+
+export interface ApplyProjectRequestCreatedNotification
+  extends ApplyProjectRequest {
+  type: 'project.role.application.created';
+}
+
+export interface MessageReceiveNotification extends BaseNotification {
+  type: 'message_received';
+  payload: {
+    userId: string;
+    messageId: string;
+    text: string;
+  };
+}
+
+// Interface pour l'événement émis lors de la création d'une candidature
+export interface ProjectRoleApplicationCreatedEvent {
+  projectOwnerId: string;
+  applicantId: string;
+  applicantName: string;
+  projectId: string;
+  projectTitle: string;
+  projectShortDescription: string;
+  projectImage?: string;
+  projectAuthor: Profile;
+  roleName: string;
+  projectRole: ProjectRole;
+  applicationId: string;
+  selectedKeyFeatures: { feature: string }[];
+  selectedProjectGoals: { goal: string }[];
+  motivationLetter?: string;
+  message: string;
+}
