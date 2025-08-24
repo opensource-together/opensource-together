5d96da0881070c154f3623e1eb8697284d3f8d11
Olyxz16 <cedric.sazos@tutanota.com>
2025-08-24 12:49:48 +0200

    refac: add necessary modules

--- apps/server/src/features/github/github.module.ts
+++ apps/server/src/features/github/github.module.ts
@@ -9,6 +9,8 @@ import {
   GITHUB_REPOSITORY,
 } from './repositories/github.repository.interface';
 import { GithubRepository } from './repositories/github.repository';
+import { ACCOUNT_REPOSITORY } from './repositories/account.repository.interface';
+import { AccountRepository } from './repositories/account.repository';
 
 @Module({
   imports: [HttpModule, ConfigModule, PrismaModule],
@@ -18,14 +20,19 @@ import { GithubRepository } from './repositories/github.repository';
     OctokitProvider,
     GitHubStatsService,
     {
+      provide: ACCOUNT_REPOSITORY,
+      useClass: AccountRepository
+    },
+    {
       provide: GITHUB_REPOSITORY,
       useClass: GithubRepository,
     },
   ],
   exports: [
+    ACCOUNT_REPOSITORY,
     GITHUB_REPOSITORY,
     OctokitProvider,
     GitHubStatsService,
   ],
 })
-export class GithubInfrastructure {}
+export class GithubModule {}
