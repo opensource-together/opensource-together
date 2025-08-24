e7719453e537b976f2c8b243a795543f047ebdd4
Olyxz16 <cedric.sazos@tutanota.com>
2025-08-24 12:21:52 +0200

    refac: remove /v1 in api route

--- apps/web/src/config/config.ts
+++ apps/web/src/config/config.ts
@@ -1,8 +1,6 @@
 const baseUrl = process.env.NEXT_PUBLIC_API_URL ?? "";
 
-export const API_BASE_URL: string = baseUrl.endsWith("/v1")
-  ? baseUrl
-  : `${baseUrl}/v1`;
+export const API_BASE_URL: string = baseUrl;
 
 if (!baseUrl) {
   console.warn("⚠️ NEXT_PUBLIC_API_URL is not defined! API calls will fail.");
