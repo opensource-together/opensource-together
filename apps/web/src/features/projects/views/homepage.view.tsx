91c950e1becbead746428b0c2238c7df6cf4e609
y2_znt <yonideserbaix@gmail.com>
2025-09-06 18:37:26 +0200

    feat: update project discovery hero and illustrations
    - Removed outdated Bible illustrations and replaced them with a new Bible image for improved visual consistency.
    - Refactored ProjectDiscoveryHero component to utilize the new illustration and adjusted image properties for better responsiveness.
    - Updated homepage view to reflect changes in the ProjectDiscoveryHero component structure.

--- apps/web/src/features/projects/views/homepage.view.tsx
+++ apps/web/src/features/projects/views/homepage.view.tsx
@@ -3,9 +3,8 @@
 import { EmptyState } from "@/shared/components/ui/empty-state";
 import PaginationNavigation from "@/shared/components/ui/pagination-navigation";
 
-import ProjectDiscoveryHero from "@/features/projects/components/project-discovery-hero.component";
-
 import HomepageError from "../components/error-ui/homepage-error.component";
+import ProjectDiscoveryHero from "../components/project-discovery-hero.component";
 import ProjectGrid from "../components/project-grid.component";
 import SkeletonHomepageViews from "../components/skeletons/skeleton-homage-view.component";
 import { useProjects } from "../hooks/use-projects.hook";
@@ -17,26 +16,27 @@ export default function HomepageView() {
   if (isError || !projects) return <HomepageError />;
 
   return (
-    <div className="mx-6 max-w-6xl space-y-4 md:space-y-5 lg:mx-auto">
+    <div>
       <div className="mt-8 flex flex-col items-center">
         <ProjectDiscoveryHero />
       </div>
-
-      <div className="py-4 md:py-8">
-        {projects.length === 0 ? (
-          <EmptyState
-            title="Aucun Projet Disponible"
-            description="Aucun projet n'a été trouvé pour le moment. Veuillez réessayer plus tard."
-            className="mb-28"
-          />
-        ) : (
-          <>
-            <ProjectGrid projects={projects} />
-            <div className="mt-25 mb-50">
-              <PaginationNavigation />
-            </div>
-          </>
-        )}
+      <div className="mx-6 max-w-6xl space-y-4 md:space-y-5 lg:mx-auto">
+        <div className="py-4 md:py-8">
+          {projects.length === 0 ? (
+            <EmptyState
+              title="Aucun Projet Disponible"
+              description="Aucun projet n'a été trouvé pour le moment. Veuillez réessayer plus tard."
+              className="mb-28"
+            />
+          ) : (
+            <>
+              <ProjectGrid projects={projects} />
+              <div className="mt-25 mb-50">
+                <PaginationNavigation />
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
