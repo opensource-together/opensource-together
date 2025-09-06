1069fdda3472b047e751bd034e854652ec216421
y2_znt <yonideserbaix@gmail.com>
2025-09-06 19:17:32 +0200

    refactor: update styling and layout in ProjectCard and StackLogo components
    - Adjusted the size and background color of the StackLogo component for improved visual consistency.
    - Enhanced text styling in ProjectCard for better readability, including font weight adjustments and spacing refinements.
    - Modified layout of tech stack display in ProjectCard to improve alignment and spacing.

--- apps/web/src/shared/components/logos/stack-logo.tsx
+++ apps/web/src/shared/components/logos/stack-logo.tsx
@@ -29,11 +29,11 @@ export default function StackLogo({
   return (
     <div className="flex flex-wrap gap-2">
       <div className="flex items-center gap-1">
-        <div className="flex size-6 items-center justify-center rounded-full bg-black/5 p-1">
+        <div className="flex size-5 items-center justify-center rounded-full bg-[#FAFAF9] p-1">
           <Image src={iconSrcToUse} alt={alt} width={width} height={height} />
         </div>
         {name && (
-          <span className="text-sm tracking-tighter text-black/70">{name}</span>
+          <span className="text-sm font-medium text-black/70">{name}</span>
         )}
       </div>
     </div>
