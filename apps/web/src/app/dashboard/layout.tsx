721bd1ab8495ed227a34c35ca97ed156bb58a6b3
y2_znt <yonideserbaix@gmail.com>
2025-09-07 23:13:54 +0200

    style: improve layout and spacing in dashboard components
    - Adjusted the height calculation in DashboardLayout for better responsiveness.
    - Enhanced the structure of DashboardSidebar to ensure consistent spacing and alignment.
    - Moved modal footer rendering in StepFiveForm and StepThreeForm for improved layout consistency.

--- apps/web/src/app/dashboard/layout.tsx
+++ apps/web/src/app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import DashboardSidebar from "@/features/dashboard/components/layout/dashboard-s
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex h-[calc(100vh-65px)] overflow-hidden md:h-[calc(100vh-85px)]">
+    <div className="flex h-[calc(100vh-65px)] overflow-hidden md:h-[calc(99vh-85px)]">
       <DashboardSidebar />
       <main className="mx-7 flex-1 overflow-y-auto pt-8 lg:mx-0 lg:ml-52">
         <div className="max-w-[708px]">{children}</div>
