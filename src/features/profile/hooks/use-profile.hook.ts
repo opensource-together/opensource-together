5d71a52580bc91a4ab31a565aa5cc0407792ae2b
y2_znt <yonideserbaix@gmail.com>
2025-10-07 20:00:40 +0200

    feat: add logo update functionality and refactor profile update logic

--- src/features/profile/hooks/use-profile.hook.ts
+++ src/features/profile/hooks/use-profile.hook.ts
@@ -41,17 +41,13 @@ export const useProfileUpdate = () => {
     mutationFn: ({
       id,
       updateData,
-      avatarFile,
-      shouldDeleteAvatar,
     }: {
       id: string;
       updateData: ProfileSchema;
-      avatarFile?: File;
-      shouldDeleteAvatar?: boolean;
-    }) => updateProfile(id, updateData, avatarFile, shouldDeleteAvatar),
-    loadingMessage: "Mise à jour de votre profil en cours...",
-    successMessage: "Profil mis à jour avec succès",
-    errorMessage: "Erreur lors de la mise à jour du profil",
+    }) => updateProfile(id, updateData),
+    loadingMessage: "Updating your profile...",
+    successMessage: "Profil updated with success",
+    errorMessage: "Error updating your profile",
     options: {
       onSuccess: () => {
         queryClient.invalidateQueries({ queryKey: ["user/me"] });
@@ -68,7 +64,7 @@ export const useProfileUpdate = () => {
 };
 
 /**
- * Hook to update the logo of a user by their ID.
+ * Hook to fetch the authenticated user's pull requests with pagination and filters.
  *
  * @param id - The ID of the user to update.
  * @param file - The file to update the logo with.
