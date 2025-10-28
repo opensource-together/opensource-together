358d8d3b51c5a29b90a40e92e170ddeff93c5088
y2_znt <yonideserbaix@gmail.com>
2025-10-28 11:33:45 +0100

    style: adjust text sizes in HeroBadge component for better readability
    - Updated text sizes in the HeroBadge component to improve visual consistency and readability across different screen sizes.

--- src/shared/components/ui/hero-badge.tsx
+++ src/shared/components/ui/hero-badge.tsx
@@ -11,10 +11,10 @@ const HeroBadge: React.FC<BadgeProps> = ({ className = "" }) => {
       className={`hover:bg-accent inline-flex items-center justify-between rounded-full border border-black/5 bg-white py-1 pr-2 pl-1 tracking-tight transition-colors ${className}`}
     >
       <div className="flex items-center gap-2">
-        <span className="text-ost-blue-three bg-ost-blue-one/10 ml-0.5 rounded-full px-3 py-1 text-xs leading-tight font-medium">
+        <span className="text-ost-blue-three bg-ost-blue-one/10 ml-0.5 rounded-full px-3 py-1 text-[10px] leading-tight font-medium md:text-xs">
           Update
         </span>
-        <span className="text-muted-foreground text-xs leading-tight font-medium text-nowrap">
+        <span className="text-muted-foreground text-[10px] leading-tight font-medium text-nowrap md:text-xs">
           Join a community of active contributers
         </span>
       </div>
