74666f3e73e7dc550134b8095f712bb9fe18b32c
y2_znt <yonideserbaix@gmail.com>
2025-11-06 19:30:02 +0100

    feat: add CookieConsent component for user privacy management
    - Introduced a new CookieConsent component to manage user consent for cookies, enhancing compliance with privacy regulations.
    - Integrated the CookieConsent component into the Providers to ensure it is displayed across the application.
    - Added a Card component for consistent styling of the CookieConsent UI.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,6 +5,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
 import { ReactNode } from "react";
 
+import { CookieConsent } from "@/shared/components/blocks/cookie-consent";
 import { getQueryClient } from "@/shared/lib/query-client";
 
 export function Providers({ children }: { children: ReactNode }) {
@@ -14,6 +15,7 @@ export function Providers({ children }: { children: ReactNode }) {
     <QueryClientProvider client={queryClient}>
       <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
       <ReactQueryDevtools initialIsOpen={false} />
+      <CookieConsent variant="mini" />
     </QueryClientProvider>
   );
 }
