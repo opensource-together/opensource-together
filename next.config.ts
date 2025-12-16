8fddce4cbbc42d44835e8f75ed62e673f5acc0e4
y2_znt <yonideserbaix@gmail.com>
2025-12-16 22:46:03 +0100

    refactor: rename next.config.js -> next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "standalone",
   images: {
+    qualities: [25, 50, 75, 85, 100],
     remotePatterns: [
       {
         protocol: "https",
